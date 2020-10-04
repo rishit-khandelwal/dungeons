98592c0272c7fd30bcadbd8ea07b280fa09b6086
Rishit Khandelwal <65018379+rishit-khandelwal@users.noreply.github.com>
2020-10-04 10:39:20 +0530

    Perspective of the player

--- index.js
+++ index.js
@@ -77,19 +77,23 @@ class Obstacle {
 }
 
 class Player {
-    constructor(x,y,vr) {
+    constructor(x,y,vr,props={}) {
         this.pos = createVector(x,y);
         this.range = vr;
+        this.props = props
+        this.props['vision'] = this.range/width
     }
 
     move(x,y) {
-        this.pos = createVector(this.pos.x+x,this.pos.y+y);
+        for (let obj of objs.concat(powerups))
+            obj.pos = createVector(obj.pos.x+x,obj.pos.y+y);
     }
 
     up(trait) {
         switch (trait) {
             case 'vision':
-                this.range += 10;
+                if (this.range < 100)
+                    this.range += 10;
             break;
 
             case '-vision':
@@ -105,6 +109,24 @@ class Player {
     show() {
         fill(255);
         stroke(255);
+
+        if (this.pos.x > width) {
+            this.pos.x = 10
+        }
+        
+        if (this.pos.x < 0 ) {
+            this.pos.x = width-10
+        }
+
+        if (this.pos.y < 0 ) {
+            this.pos.y = height-10
+        }
+
+        if (this.pos.y > height ) {
+            this.pos.y = 10
+        }
+
+        this.props['vision'] = this.range/width
 
         for (let obj of objs) {
             if (
@@ -123,13 +145,9 @@ class Player {
                 dist(
                     this.pos.x,this.pos.y,
                     obj.pos.x,obj.pos.y,
-                ) < 10
+                ) < 20
             ) {
                 obj.show();
-                this.move(
-                    this.pos.x-obj.pos.x,
-                    this.pos.y-obj.pos.y
-                )
             }
         }
 
@@ -148,6 +166,13 @@ class Player {
         stroke(29,155,203)
 
         rect(
+            this.pos.x - 14 + 1/this.props['vision'],
+            this.pos.y - 30,
+            80*this.props['vision'],
+            5
+        )
+
+        rect(
             this.pos.x-5,
             this.pos.y-5,
             10,10
@@ -160,7 +185,7 @@ let charecter;
 function setup() {
     createCanvas(400,400);
 
-    charecter = new Player(10,10,50);
+    charecter = new Player(200,200,50);
 
     for (let i = 0; i < 10 ; i++ ) {
         new Obstacle(
@@ -175,28 +200,33 @@ function setup() {
 document.addEventListener('keydown', (e) => {
     
     switch (e.key) {
-        case 'w':
+        case 's':
             charecter.move(0,-10)
         break;
 
-        case 'a':
+        case 'd':
             charecter.move(-10,0)
         break;
 
-        case 's':
+        case 'w':
             charecter.move(0,10)
         break;
 
-        case 'd':
+        case 'a':
             charecter.move(10,0)
         break;
     }
 })
 
+function mousePressed() {
+    charecter.range = width
+}
+
 function draw() {
-    background(0);
+    background(128);
     for (let pu of powerups) {
         pu.update(charecter)
     }
+    
     charecter.show();
 }
